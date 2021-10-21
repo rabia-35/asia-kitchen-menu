3127058bf66d4a54dd56c7517f0ad3640c019f9b
rabia ölker <rabia.olker@gmail.com>
2021-10-21 13:27:17 +0300

    menüdeki bilgiler obje olarak eklend

---
+++ asian.js
@@ -0,0 +1,74 @@
+let menu=[
+    {
+        id: "tteok" ,
+        title: "Tteokbokki",
+        category:"korea",
+        price: 10.99,
+        img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl.jpg ",
+        derc:"Spicy rice cakes, serving with fish cake."
+    }, 
+     {
+        id: "chick" ,
+        title: "Chicken Ramen",
+        category:"japan",
+        price: 7.99,
+        img: "https://www.kitchensanctuary.com/wp-content/uploads/2020/12/Quick-Chicken-Ramen-square-FS-22-500x500.jpg",
+        derc: "Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg."
+    },
+
+    {
+        id: "bibim" ,
+        title: "Bibimbap",
+        category:"korea",
+        price: 8.99,
+        img: "https://i.lezzet.com.tr/images-xxlarge-recipe/bibimbap-2d68e268-6c19-4ff8-b5f8-1145372450cf.jpg",
+        derc:"Boiling vegetables, serving with special hot sauce"
+    },
+     {
+        id: "danda" ,
+        title: "Dan Dan Mian",
+        category:"china",
+        price: 5.99 ,
+        img: "https://i2.wp.com/seonkyounglongest.com/wp-content/uploads/2019/10/Dan-Dan-Noodles-11.jpg?fit=2000%2C1333&ssl=1",
+        derc:"Dan dan noodle, serving with green onion"
+    },
+     {
+        id: "yangz" ,
+        title: "Yangzhou Fried Rice",
+        category:"china",
+        price: 12.99,
+        img: "https://www.chinayummyfood.com/wp-content/uploads/2019/09/Yang-Chow-Fried-Rice-500x500.jpg",
+        derc:"Yangzhou style fried rice, serving with bean and pickles"
+    },
+     {
+        id: "onigi" ,
+        title: "Onigiri",
+        category:"japan",
+        price: 9.99 ,
+        img: "https://www.asianfoodlovers.com/media/catalog/product/cache/8/image/750x750/9df78eab33525d08d6e5fb8d27136e95/o/n/onigiri.jpg",
+        derc:"Rice Sandwich, serving with soy sauce"
+    },
+    {
+         id: "jajan" ,
+         title: "Jajangmyeon",
+         category:"korea",
+         price: 15.99,
+         img: "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-735x613.jpg"
+    },
+     {
+        id: "mayis" ,
+        title: "Ma Yi Shang Shu",
+        category:"china",
+        price: 12.99,
+        img: "Hot pepper sauce noodle, serving with soy bean and onion",
+        derc:"Hot pepper sauce noodle, serving with soy bean and onion"
+    },
+     {
+        id: "doroy" ,
+        title: "Doroyaki",
+        category:"japan",
+        price: 3.99,
+        img: "https://www.pratikevyemekleri.com/wp-content/uploads/2021/03/Dorayaki-tarifi.jpg",
+        derc:"Red bean paste dessert, serving with honey."
+    }
+]
